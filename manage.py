7a2a414d63fac766ecf7efb55368a7d780feab1f
Bill Mitchell <wkmitch@gmail.com>
2017-11-27 19:18:16 -0800

    first commit

---
+++ manage.py
@@ -0,0 +1,9 @@
+# mange.py
+
+from flask_script import Manager
+from project import app
+
+manager = Manager(app)
+
+if __name__ == '__main__':
+    manager.run()
---
+++ project/config.py
@@ -0,0 +1,14 @@
+class BaseConfig:
+    DEBUG = False
+    TESTING = False
+
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
+
+class TestingConfig(BaseConfig):
+    DEBUG = True
+    TESTING = True
+
+class ProductionConfig(BaseConfig):
+    DEBUG = False
